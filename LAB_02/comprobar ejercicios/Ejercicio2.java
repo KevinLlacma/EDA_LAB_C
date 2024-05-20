8456407b044b11677ac8769e55932ff5465c7249
KevinLlacma <90292023+KevinLlacma@users.noreply.github.com>
2024-05-20 18:41:06 -0500

    comprobacion y desarrollo del primer ejercicio

---
+++ LAB_02/comprobar ejercicios/Ejercicio2.java
@@ -0,0 +1,31 @@
+public class Ejercicio2 {
+    
+public static void main(String[] args)
+{
+    //cantidad de dígitos
+    int n = 5;
+    incre(n);
+}
+//método que imprime dígitos de 1 hasta n
+//ejemplo: n=5 -> 12345
+//ejemplo: n=8 -> 12345678
+static void incre(int n)
+{
+    if(n>0)
+    {
+        incre(n-1);
+        System.out.print(n);
+    }
+    else
+        System.out.println();
+}
+
+
+}
+
+
+
+
+
+
+
